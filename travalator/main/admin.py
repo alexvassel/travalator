2c34405cf40c5a3bda3a0eb7a7056a5dce96b070
Aleksandr Vasilev <vasilev@bradburylab.com>
2016-06-08 14:46:55 +0300

    update admin

--- travalator/main/admin.py
+++ travalator/main/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from .models import RoutePoint
+from .models import RoutePoint, Route
 
 # Register your models here.
 
 
+@admin.register(RoutePoint)
 class RoutePointAdmin(admin.ModelAdmin):
     pass
 
-admin.site.register(RoutePoint, RoutePointAdmin)
+
+@admin.register(Route)
+class RouteAdmin(admin.ModelAdmin):
+    pass
