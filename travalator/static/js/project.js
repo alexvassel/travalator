47c06833686f71a74613a4542fd8898f02ba401c
Aleksandr Vasilev <vasilev@bradburylab.com>
2016-06-09 17:35:52 +0300

    introduce google maps

--- travalator/static/js/project.js
+++ travalator/static/js/project.js
@@ -1,1 +1,15 @@
 /* Project specific Javascript goes here. */
+
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    initMap();
+});
+
+function initMap() {
+  // Create a map object and specify the DOM element for display.
+  var map = new google.maps.Map(document.getElementById('map_canvas'), {
+    center: {lat: -34.397, lng: 150.644},
+    scrollwheel: false,
+    zoom: 8
+  });
+}
