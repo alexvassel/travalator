2c270f55b4775be7f778f5cc0c6ea706d5bca7ec
Aleksandr Vasilev <vasilev@bradburylab.com>
2016-06-09 18:39:38 +0300

    route points as json

--- travalator/main/urls.py
+++ travalator/main/urls.py
@@ -2,6 +2,7 @@
 from __future__ import absolute_import, unicode_literals
 
 from django.conf.urls import url
+from jsonview.decorators import json_view
 
 from . import views
 
@@ -11,6 +12,12 @@ urlpatterns = [
         regex=r'^$',
         view=views.IndexView.as_view(),
         name='index'
+    ),
+
+    url(
+        regex=r'^route/(?P<route_pk>[0-9]+)/points/$',
+        view=json_view(views.PointsListView.as_view()),
+        name='route_points'
     ),
 
 ]
--- travalator/main/views.py
+++ travalator/main/views.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import JsonResponse, Http404
+from django.shortcuts import get_object_or_404
+from django.views.generic import DetailView, View
 
-# Create your views here.
-from django.views.generic import DetailView
-
-from .models import Route
+from .models import Route, RoutePoint
 
 
 class IndexView(LoginRequiredMixin, DetailView):
@@ -13,3 +13,18 @@ class IndexView(LoginRequiredMixin, DetailView):
 
     def get_object(self, queryset=None):
         return self.model.objects.order_by('?').first()
+
+
+class PointsListView(LoginRequiredMixin, View):
+    model = RoutePoint
+
+    def get(self, *args, **kw):
+        response = dict(points=[])
+        route = get_object_or_404(Route, pk=kw.get('route_pk'))
+        for p in route.points.all():
+            point = dict(location=p.location.get_coords())
+            point['name'] = p.name
+            point['description'] = p.description
+            response['points'].append(point)
+        return JsonResponse({'data': response})
+
