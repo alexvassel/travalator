c20871e3651b98b3967627541c57a7c499880bb8
Aleksandr Vasilev <vasilev@bradburylab.com>
2016-06-09 14:27:18 +0300

    started new app (main)

--- travalator/main/views.py
+++ travalator/main/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
+from django.views.generic import DetailView
+
+from .models import Route
+
+
+class IndexView(LoginRequiredMixin, DetailView):
+    model = Route
+    template_name = 'main/index.html'
+
+    def get_object(self, queryset=None):
+        return self.model.objects.order_by('?').first()
