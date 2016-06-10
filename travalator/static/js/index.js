efae8900d3bb23e02be04ddf4141e660ab8988b2
Aleksandr Vasilev <vasilev@bradburylab.com>
2016-06-10 12:58:36 +0300

    updated js

---
+++ travalator/static/js/index.js
@@ -0,0 +1,49 @@
+/**
+ * Created by vasilev on 10.06.16.
+ */
+
+$( document ).ready(function() {
+    var mapElement = document.getElementById('map_canvas');
+    var map = initMap(mapElement);
+
+    $.ajax({
+        url: $(mapElement).data('points')
+    }).done(function(response) {
+        if (response.error !== undefined) {
+            console.log(response.message);
+            return
+        }
+        setMarkers(response.data.points, map);
+    });
+});
+
+function initMap(mapElement) {
+    var centerPoint = {lat: $(mapElement).data('lat'), lng: $(mapElement).data('lng')};
+    return new google.maps.Map(mapElement, {
+    center: centerPoint,
+    scrollwheel: true,
+    zoom: 6
+  });
+}
+
+function setMarkers(points, map) {
+    $.each(points, function(_, point) {
+
+        var marker = new google.maps.Marker({
+            position: {lat: point.location[1], lng: point.location[0]},
+            title: point.name,
+            animation: google.maps.Animation.DROP
+        });
+        setMarkerInfo(point, marker, map);
+        marker.setMap(map);
+    });
+}
+
+function setMarkerInfo(point, marker, map) {
+    var infowindow = new google.maps.InfoWindow({
+            content: point.description
+    });
+    marker.addListener('click', function() {
+        infowindow.open(map, marker);
+    });
+}
--- travalator/static/js/project.js
+++ travalator/static/js/project.js
@@ -1,48 +1,4 @@
 /* Project specific Javascript goes here. */
 
 // A $( document ).ready() block.
-$( document ).ready(function() {
-    var mapElement = document.getElementById('map_canvas');
-    var map = initMap(mapElement);
 
-    $.ajax({
-        url: '/route/' + $(mapElement).data('route') + '/points/'
-    }).done(function(response) {
-        if (response.error !== undefined) {
-            console.log(response.message);
-            return
-        }
-        setMarkers(response.data.points, map);
-    });
-});
-
-function initMap(mapElement) {
-    var centerPoint = {lat: $(mapElement).data('lat'), lng: $(mapElement).data('lng')};
-    return new google.maps.Map(mapElement, {
-    center: centerPoint,
-    scrollwheel: true,
-    zoom: 6
-  });
-}
-
-function setMarkers(points, map) {
-    $.each(points, function(_, point) {
-
-        var marker = new google.maps.Marker({
-            position: {lat: point.location[1], lng: point.location[0]},
-            title: point.name,
-            animation: google.maps.Animation.DROP,
-        });
-        setMarkerInfo(point, marker, map);
-        marker.setMap(map);
-    });
-}
-
-function setMarkerInfo(point, marker, map) {
-    var infowindow = new google.maps.InfoWindow({
-            content: point.description
-    });
-    marker.addListener('click', function() {
-        infowindow.open(map, marker);
-    });
-}
