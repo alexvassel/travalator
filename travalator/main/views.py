c12bf894a2d2ec60dbc55b35adea967c61cdcdc4
Aleksandr Vasilev <vasilev@bradburylab.com>
2016-09-22 10:10:58 +0300

    cache added

--- travalator/main/views.py
+++ travalator/main/views.py
@@ -2,6 +2,9 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django.views.generic import DetailView, View
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_cookie
 
 from .models import Route
 
@@ -14,10 +17,20 @@ class IndexView(LoginRequiredMixin, DetailView):
     def get_object(self, queryset=None):
         return self.model.objects.prefetch_related('points').order_by('?').first()
 
+    @method_decorator(cache_page(60*60))
+    @method_decorator(vary_on_cookie)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
 
 class PointsListView(LoginRequiredMixin, View):
 
     def get(self, *args, **kw):
         route = get_object_or_404(Route, pk=kw.get('route_pk'))
         return JsonResponse({'data': route.formatted_points})
+
+    @method_decorator(cache_page(60*60))
+    @method_decorator(vary_on_cookie)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
 
