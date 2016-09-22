3849fa70cb16bfa2d05efa52d55c3624d02ae770
Aleksandr Vasilev <vasilev@bradburylab.com>
2016-09-22 17:42:35 +0300

    added factories

--- travalator/users/tests/factories.py
+++ travalator/users/tests/factories.py
@@ -1,11 +1,25 @@
 import factory
 
 
-class UserFactory(factory.django.DjangoModelFactory):
+class BaseUserFactory(factory.django.DjangoModelFactory):
     username = factory.Sequence(lambda n: 'user-{0}'.format(n))
     email = factory.Sequence(lambda n: 'user-{0}@example.com'.format(n))
     password = factory.PostGenerationMethodCall('set_password', 'password')
 
+
+class UserFactory(BaseUserFactory):
     class Meta:
         model = 'users.User'
+        django_get_or_create = ('username', )
+
+
+class CompanyFactory(BaseUserFactory):
+    class Meta:
+        model = 'users.Company'
+        django_get_or_create = ('username', )
+
+
+class TouristFactory(BaseUserFactory):
+    class Meta:
+        model = 'users.Tourist'
         django_get_or_create = ('username', )
