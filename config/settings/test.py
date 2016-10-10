469cc9e3d58b15265d27772c51ff16697d135878
Aleksandr Vasilev <vasilev@bradburylab.com>
2016-10-10 17:49:43 +0300

    added settings for tests

---
+++ config/settings/test.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from .common import *  # noqa
+
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'
+
+SECRET_KEY = env('DJANGO_SECRET_KEY', default='8xr3)ik4)@aq%ty=h5nq-+0c5cbt#@*t4)%wi&whmdzc&d83%d')
